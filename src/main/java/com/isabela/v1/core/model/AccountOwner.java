60cf5ec175cf05906b7adb5df694f1d01784ecda
Isabela Hodorog <isabela.hodorog@gmail.com>
2018-04-17 20:03:58 +0300

    create entity

---
+++ src/main/java/com/isabela/v1/core/model/AccountOwner.java
@@ -0,0 +1,33 @@
+package com.isabela.v1.core.model;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@Table(name = "account_owner")
+public class AccountOwner {
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    @Setter(AccessLevel.NONE)
+    @Column(name = "ao_id")
+    private Long id;
+
+    @Column(name = "ao_name")
+    private String name;
+
+    @Column(name = "ao_email")
+    private String email;
+
+    @Column(name = "ao_password")
+    private String password;
+}
